46016b2dbf4f30692994b94a841ed60d9813bdf9
fyrodems <fyrodems@gmail.com>
2023-03-14 20:43:40 +0100

    cra + cleaning

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Login from "./Login";
+const App = () => {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./css/login.css";
+
+const Login = () => {
+  return <div className="login">Login</div>;
+};
+
+export default Login;
