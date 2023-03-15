ec726ee0e30f74f1dfc222318e37cfb517ddc917
fyrodems <fyrodems@gmail.com>
2023-03-15 19:49:23 +0100

    get data using token

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import Login from "./Login";
 import { getTokenFromResponseUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
+console.log(spotify);
 
 const App = () => {
   const [token, setToken] = useState(null);
@@ -13,8 +17,14 @@ const App = () => {
 
     if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+      console.log(_token);
+
+      spotify.getMe().then((res) => {
+        console.log(res);
+      });
     }
-  }, []);
+  }, [token]);
 
   return <div className="app">{token ? <></> : <Login />}</div>;
 };
