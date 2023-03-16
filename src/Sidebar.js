b4821d3f8084a0b4c4897171ceb919be7641d5c2
fyrodems <fyrodems@gmail.com>
2023-03-16 21:33:24 +0100

    add sidebar option

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,19 @@
 import "./css/sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        alt="logo"
+        src="https:getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+      />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ src/SidebarOption.js
@@ -0,0 +1,7 @@
+import "./css/sidebar-option.css";
+
+const SidebarOption = ({ title, Icon }) => {
+  return <div className="sidebar-option">{title}</div>;
+};
+
+export default SidebarOption;
