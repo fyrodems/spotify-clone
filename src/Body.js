5351d59fcd0b74da7d8a835a623b7f9f7082a23a
fyrodems <fyrodems@gmail.com>
2023-03-18 16:34:28 +0100

    finished banner styling

--- src/Body.js
+++ src/Body.js
@@ -5,6 +5,14 @@ const Body = ({ spotify }) => {
   return (
     <div className="body">
       <Header spotify={spotify} />
+      <div className="body__banner">
+        <img src="" alt="" />
+        <div className="banner__text">
+          <strong>playlist</strong>
+          <h2>discover weekly</h2>
+          <p>desc</p>
+        </div>
+      </div>
     </div>
   );
 };
