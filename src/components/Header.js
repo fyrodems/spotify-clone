47b93bd581d6b9d03825d7a9c054824e2944936c
fyrodems <fyrodems@gmail.com>
2023-03-25 19:49:28 +0100

    pass data from search to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import "../css/header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import { Avatar } from "@material-ui/core";
@@ -9,25 +9,16 @@ const Header = ({ spotify }) => {
   const useDataLayerValue = useContext(DataLayerContext);
   const [{ user }] = useDataLayerValue;
 
-  // useEffect(() => {
-  //   const pull_data = (data) => {
-  //     console.log(data); // LOGS DATA FROM CHILD (My name is Dean Winchester... &)
-  //     // return data;
-  //   };
-
-  //   return pull_data;
-  // }, [data]);
-
-  const pull_data = (data) => {
-    console.log(data); // LOGS DATA FROM CHILD (My name is Dean Winchester... &)
-    // return data;
+  const getSearchResults = (data) => {
+    console.log(data);
+    return data;
   };
 
   return (
     <div className="header">
       <div className="header__search">
         <SearchIcon />
-        <SearchEngine func={pull_data} spotify={spotify} />
+        <SearchEngine handleSearch={getSearchResults} spotify={spotify} />
       </div>
       <div className="header__user">
         <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
--- src/utils/searchEngine.js
+++ src/utils/searchEngine.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const SearchEngine = ({ func, spotify }) => {
+const SearchEngine = (props) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
@@ -8,23 +8,22 @@ const SearchEngine = ({ func, spotify }) => {
     setSearchQuery(e.target.value);
   };
 
-  const handleSearch = (e) => {
+  const handleSearchFunction = (e) => {
     if (e.keyCode !== 13) return;
 
-    spotify.searchTracks(searchQuery).then((response) => {
-      //   console.log(response);
+    props.spotify.searchTracks(searchQuery).then((response) => {
+      // console.log(response);
       setSearchResults(response);
+      // console.log(searchResults);
     });
   };
-
-  func = searchResults;
 
   return (
     <input
       type="text"
       placeholder="Search for something"
       onChange={getUserQuery}
-      onKeyUp={handleSearch}
+      onKeyUp={props.handleSearch(handleSearchFunction)}
       //   searchResults={searchResults}
     />
   );
